86c8bc5f593f667ba653424110ffd1b5db95f0f4
Batuhan Taskaya <batuhan@python.org>
2021-07-23 21:03:12 +0300

    common: introduce apply_condition/is_truthy

--- refactor/common.py
+++ refactor/common.py
@@ -2,11 +2,20 @@ import ast
 
 
 def negate(node: ast.expr) -> ast.UnaryOp:
-    """Negate the given `node`.
-
-    Example:
-        input => `foo`
-        output => `not foo`
-    """
-
+    """Negate the given `node`."""
     return ast.UnaryOp(op=ast.Not(), operand=node)
+
+
+def apply_condition(condition: bool, node: ast.expr) -> ast.expr:
+    """Negate the node if `condition` is a falsy value."""
+    if condition:
+        return node
+    else:
+        return negate(node)
+
+
+def is_truthy(op: ast.cmpop) -> bool:
+    """Return `True` for comparison operators that
+    depend on truthness (`==`, `is`, `in`), `False`
+    for others."""
+    return isinstance(op, (ast.Eq, ast.In, ast.Is))
