c3e2b43c1a184e8e42246e90ebfe6af8c87a6022
Batuhan Taskaya <batuhan@python.org>
2021-07-23 21:03:46 +0300

    session: implement run/run_file endpoints

--- refactor/ast.py
+++ refactor/ast.py
@@ -1,10 +1,15 @@
 import ast
-from typing import Optional, Protocol, runtime_checkable
+from typing import List, Optional, Protocol, runtime_checkable
 
 
 @runtime_checkable
-class Node(Protocol):
+class PositinalNode(Protocol):
     lineno: int
     col_offset: int
     end_lineno: int
     end_col_offset: int
+
+
+def split_lines(source: str) -> List[str]:
+    # TODO: https://github.com/python/cpython/blob/83d1430ee5b8008631e7f2a75447e740eed065c1/Lib/ast.py#L299-L321
+    return source.splitlines()
--- refactor/core.py
+++ refactor/core.py
@@ -2,43 +2,45 @@ from __future__ import annotations
 
 import ast
 import copy
+import tokenize
 from contextlib import suppress
 from dataclasses import dataclass, field
-from typing import List, Optional, cast
+from pathlib import Path
+from typing import List, Optional, Tuple, cast
 
-from refactor.ast import Node
+from refactor.ast import PositinalNode, split_lines
+from refactor.change import Change
 
 
 @dataclass
 class Action:
-    node: Node
+    node: ast.AST
 
     def apply(self, source: str) -> str:
         """Refactor a source segment in the given string."""
-        lines = source.splitlines()
+        lines = split_lines(source)
         view = slice(self.node.lineno - 1, self.node.end_lineno)
 
         target_lines = lines[view]
         start_prefix = target_lines[0][: self.node.col_offset]
         end_prefix = target_lines[-1][self.node.end_col_offset :]
 
-        replacement_node = cast(ast.AST, self.build())
-        replacement = ast.unparse(replacement_node).splitlines()
+        replacement = ast.unparse(self.build()).splitlines()
         replacement[0] = start_prefix + replacement[0]
         replacement[-1] += end_prefix
 
         lines[view] = replacement
         return "\n".join(lines)
 
-    def build(self) -> Node:
+    def build(self) -> ast.AST:
         return self.node
 
-    def branch(self) -> Node:
+    def branch(self) -> ast.AST:
         return copy.deepcopy(self.node)
 
 
 class Rule:
-    def match(self, node: Node) -> Optional[Action]:
+    def match(self, node: ast.AST) -> Optional[Action]:
         """Match the node against the current refactoring rule.
 
         On success, it will return an `Action` instance. On fail
@@ -52,19 +54,38 @@ class Session:
 
     rules: List[Rule] = field(default_factory=list)
 
-    def run(self, source: str) -> str:
-        """Refactor the given string with the rules bound to
-        this session."""
-
+    def _run(self, source: str, *, _changed: bool = False) -> Tuple[str, bool]:
         tree = ast.parse(source)
 
         for node in ast.walk(tree):
-            if not isinstance(node, Node):
+            if not isinstance(node, PositinalNode):
                 continue
 
             for rule in self.rules:
                 with suppress(AssertionError):
                     if action := rule.match(node):
-                        return self.run(action.apply(source))
+                        return self._run(action.apply(source), _changed=True)
 
+        return source, _changed
+
+    def run(self, source: str) -> str:
+        """Refactor the given string with the rules bound to
+        this session."""
+
+        source, _ = self._run(source)
         return source
+
+    def run_file(self, file: Path) -> Optional[Change]:
+        """Refactor the given file, and return a Change object
+        containing the refactored version. If nothing changes, return
+        None."""
+
+        with tokenize.open(file) as stream:
+            source = stream.read()
+
+        new_source, is_changed = self._run(source)
+
+        if is_changed:
+            return Change(file, source, new_source)
+        else:
+            return None
