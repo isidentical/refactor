8fc95f45a4a1f112afe4aab4f9a23c3fbe7d7b77
Batuhan Taskaya <batuhan@python.org>
2021-07-31 16:42:27 +0300

    context: implement a way to retrieve definitions from scopes

--- refactor/common.py
+++ refactor/common.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import ast
-from functools import lru_cache
-from typing import Any, Dict, Optional, Tuple, Type, cast
+from collections import deque
+from functools import cache, singledispatch
+from typing import Any, Dict, Iterator, List, Optional, Tuple, Type, cast
 
 
 def negate(node: ast.expr) -> ast.UnaryOp:
@@ -36,11 +37,22 @@ def is_truthy(op: ast.cmpop) -> Optional[bool]:
     return _OPERATOR_MAP.get(type(op))
 
 
-def is_contextful(node: ast.AST) -> bool:
-    """Check if the node is a context starter (e.g
-    a function definition)."""
-    return isinstance(
-        node,
+@cache
+def _is_comprehension(node_type: Type[ast.AST]) -> bool:
+    return issubclass(
+        node_type, (ast.SetComp, ast.ListComp, ast.DictComp, ast.GeneratorExp)
+    )
+
+
+def is_comprehension(node: ast.AST) -> bool:
+    """Check if the node is a comprehension"""
+    return _is_comprehension(type(node))  # type: ignore
+
+
+@cache
+def _is_contextful(node_type: Type[ast.AST]) -> bool:
+    return issubclass(
+        node_type,
         (
             ast.Module,
             ast.ClassDef,
@@ -48,7 +60,15 @@ def is_contextful(node: ast.AST) -> bool:
             ast.AsyncFunctionDef,
             ast.Lambda,
         ),
+    ) or _is_comprehension(
+        node_type  # type: ignore
     )
+
+
+def is_contextful(node: ast.AST) -> bool:
+    """Check if the node is a context starter (e.g
+    a function definition)."""
+    return _is_contextful(type(node))  # type: ignore
 
 
 def pascal_to_snake(name: str) -> str:
@@ -88,7 +108,7 @@ _POSITIONAL_ATTRIBUTES = (
 _POSITIONAL_ATTRIBUTES_SET = frozenset(_POSITIONAL_ATTRIBUTES)
 
 
-@lru_cache(512)
+@cache  # type: ignore
 def has_positions(node_type: Type[ast.AST]) -> bool:
     """Return `True` if the given `node_type` tracks
     source positions."""
@@ -96,10 +116,75 @@ def has_positions(node_type: Type[ast.AST]) -> bool:
 
 
 def position_for(node: ast.AST) -> Tuple[int, int, int, int]:
+    """Return a 4-item tuple of positions for the given node."""
     positions = tuple(
         getattr(node, attribute) for attribute in _POSITIONAL_ATTRIBUTES
     )
     return cast(Tuple[int, int, int, int], positions)
+
+
+def unpack_lhs(node: ast.AST) -> Iterator[str]:
+    """Unpack assignment targets to individual identifiers"""
+    if isinstance(node, (ast.List, ast.Tuple)):
+        for element in node.elts:
+            yield from unpack_lhs(element)
+    else:
+        yield ast.unparse(node)
+
+
+def walk_scope(node: ast.AST) -> Iterator[ast.AST]:
+    """Like regular ast.walk() but only walks within the
+    current scope."""
+    assert hasattr(node, "body")
+
+    todo = deque(_walker(node))
+    while todo:
+        node = todo.popleft()
+        todo.extend(_walker(node, top_level=True))
+        yield node
+
+
+@singledispatch
+def _walker(node: ast.AST, top_level: bool = False) -> Iterator[ast.AST]:
+    yield from ast.iter_child_nodes(node)
+
+
+@_walker.register(ast.Lambda)
+@_walker.register(ast.FunctionDef)
+@_walker.register(ast.AsyncFunctionDef)
+def _walk_func(node: ast.AST, top_level: bool = False) -> Iterator[ast.AST]:
+    if top_level:
+        yield from node.decorator_list
+        yield from node.args.defaults
+        yield from node.args.kw_defaults
+        yield from _walk_optional(node.returns)
+    else:
+        yield from _walk_args(node.args)
+        yield from node.body
+
+
+@_walker.register(ast.ClassDef)
+def _walk_class(node: ast.AST, top_level: bool = False) -> Iterator[ast.AST]:
+    if top_level:
+        yield from node.decorator_list
+        yield from node.bases
+        yield from node.keywords
+    else:
+        yield from node.body
+
+
+def _walk_args(node: ast.arguments) -> List[ast.arg]:
+    args = node.posonlyargs + node.args + node.kwonlyargs
+    if node.vararg:
+        args.append(node.vararg)
+    if node.kwarg:
+        args.append(node.kwarg)
+    return args
+
+
+def _walk_optional(node: Optional[ast.AST]) -> Iterator[ast.AST]:
+    if node:
+        yield node
 
 
 class Singleton:
--- refactor/context.py
+++ refactor/context.py
@@ -18,8 +18,8 @@ from typing import (
     cast,
 )
 
+import refactor.common as common
 from refactor.ast import Unparser, UnparserBase
-from refactor.common import Singleton, is_contextful, pascal_to_snake
 
 
 class Dependable(Protocol):
@@ -92,7 +92,7 @@ class Representative:
         if self_type is Representative:
             return "<base>"
         else:
-            return pascal_to_snake(self_type.__name__)
+            return common.pascal_to_snake(self_type.__name__)
 
 
 class Ancestry(Representative):
@@ -128,13 +128,14 @@ class ScopeType(Enum):
     GLOBAL = auto()
     CLASS = auto()
     FUNCTION = auto()
+    COMPREHENSION = auto()
 
 
 @dataclass(unsafe_hash=True)
-class ScopeInfo(Singleton):
+class ScopeInfo(common.Singleton):
     node: ast.AST
     scope_type: ScopeType
-    parent_scope: Optional[ScopeInfo] = None
+    parent_scope: Optional[ScopeInfo] = field(default=None, repr=False)
 
     def can_reach(self, other: ScopeInfo) -> bool:
         if other.scope_type is ScopeType.GLOBAL:
@@ -150,6 +151,47 @@ class ScopeInfo(Singleton):
         else:
             return False
 
+    @cached_property
+    def definitions(self):
+        local_definitions = {}
+        for node in common.walk_scope(self.node):
+            if isinstance(node, ast.Assign):
+                # a, b = c = 1
+                for target in node.targets:
+                    for identifier in common.unpack_lhs(target):
+                        local_definitions[identifier] = node
+            elif isinstance(node, ast.NamedExpr):
+                # (a := b)
+                local_definitions[node.target.id] == node
+            elif isinstance(node, ast.excepthandler):
+                # except Something as err: ...
+                if node.name is not None:
+                    local_definitions[node.name] = node
+            elif isinstance(node, (ast.Import, ast.ImportFrom)):
+                # import something
+                for alias in node.names:
+                    local_definitions[alias.name] = node
+            elif isinstance(node, (ast.With, ast.AsyncWith)):
+                # with x as (y, z): ...
+                for item in node.items:
+                    if item.optional_vars:
+                        for identifier in common.unpack_lhs(item):
+                            local_definitions[identifier] = node
+            elif isinstance(node, (ast.For, ast.AsyncFor, ast.comprehension)):
+                # for a, b in c: ...
+                for identifier in common.unpack_lhs(node.target):
+                    local_definitions[identifier] = node
+            elif isinstance(
+                node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)
+            ):
+                # def something(): ...
+                local_definitions[node.name] = node
+
+        if isinstance(node, ast.arg):
+            local_definitions[node.arg] = node
+
+        return local_definitions
+
 
 class Scope(Representative):
 
@@ -160,7 +202,10 @@ class Scope(Representative):
             raise ValueError("Can't resolve Module")
 
         parents = tuple(
-            filter(is_contextful, self.context["ancestry"].get_parents(node))
+            filter(
+                common.is_contextful,
+                self.context["ancestry"].get_parents(node),
+            )
         )
 
         scope = None
@@ -173,6 +218,8 @@ class Scope(Representative):
                 parent, (ast.FunctionDef, ast.AsyncFunctionDef, ast.Lambda)
             ):
                 scope_type = ScopeType.FUNCTION
+            elif common.is_comprehension(parent):
+                scope_type = ScopeType.COMPREHENSION
 
             scope = ScopeInfo(parent, scope_type, scope)
 
