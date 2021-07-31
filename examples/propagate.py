7e030b6f15fb2c457d419ba2ee76ad803f8ec6dc
Batuhan Taskaya <batuhan@python.org>
2021-07-31 19:35:39 +0300

    examples: refactor the constant propagator with this

--- examples/propagate.py
+++ examples/propagate.py
@@ -1,55 +1,28 @@
 import ast
-from collections import defaultdict
-from typing import DefaultDict, List
 
 import refactor
-from refactor.context import Scope, ScopeInfo
+from refactor.context import Scope
 
 
-class Assignments(refactor.Representative):
+class PropagateConstants(refactor.Rule):
+
     context_providers = (Scope,)
 
-    def collect(self, scope: ScopeInfo) -> DefaultDict[str, List[ast.Assign]]:
-        assignments = defaultdict(list)
-        for node in ast.walk(scope.node):
-            # Check whether this is a simple assignment to a name
-            if (
-                isinstance(node, ast.Assign)
-                and len(node.targets) == 1
-                and isinstance(target := node.targets[0], ast.Name)
-            ):
-                # Check whether we can reach this assignment or not.
-                # For example there might be child functions where the
-                # definition is unreachable for us.
-                assignment_scope = self.context["scope"].resolve(node)
-                if scope.can_reach(assignment_scope):
-                    assignments[target.id].append(node)
-
-        return assignments
-
-
-class PropagationRule(refactor.Rule):
-
-    context_providers = (Assignments, Scope)
-
-    def match(self, node: ast.AST) -> refactor.Action:
+    def match(self, node):
         assert isinstance(node, ast.Name)
         assert isinstance(node.ctx, ast.Load)
 
-        scope = self.context["scope"].resolve(node)
-        definitions = self.context["assignments"].collect(scope)
-        assignments = definitions[node.id]
+        current_scope = self.context["scope"].resolve(node)
+        assert current_scope.defines(node.id)
 
-        # The name should be defined in the current scope
-        # and there shouldn't be any overrides
-        assert len(assignments) == 1
+        definitions = current_scope.definitions[node.id]
 
-        # The value should be a constant, so that we can safely propagate
-        [assignment] = assignments
-        assert isinstance(value := assignment.value, ast.Constant)
+        assert len(definitions) == 1
+        assert isinstance(definition := definitions[0], ast.Assign)
+        assert isinstance(value := definition.value, ast.Constant)
 
         return refactor.ReplacementAction(node, value)
 
 
 if __name__ == "__main__":
-    refactor.run(rules=[PropagationRule])
+    refactor.run(rules=[PropagateConstants])
