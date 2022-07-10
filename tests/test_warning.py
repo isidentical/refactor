703d24e7f2db2aa92800a724d36ac2f88a17fb68
Hakan Çelik <hakan-atlassian-kh70vx@deepinfo.com>
2022-07-11 00:31:56 +0300

    Fix pytest.warns(None) is deprecated warning

--- tests/test_warning.py
+++ tests/test_warning.py
@@ -1,3 +1,4 @@
+import warnings
 from types import SimpleNamespace
 from unittest.mock import patch
 
@@ -7,11 +8,19 @@ from refactor import _check_asserts
 
 
 @pytest.mark.parametrize(
-    "optimization_level, expected_warnings", [(0, 0), (1, 1), (2, 1)]
+    "optimization_level, expected_warnings", [(1, 1), (2, 1)]
 )
-def test_import_warnign(optimization_level, expected_warnings):
-    with pytest.warns(None) as record:
+def test_import_warning(optimization_level, expected_warnings):
+    with pytest.warns() as record:
         with patch("sys.flags", SimpleNamespace(optimize=optimization_level)):
             _check_asserts()
 
     assert len(record) == expected_warnings
+
+
+@pytest.mark.parametrize("optimization_level", [0])
+def test_import_no_warning(optimization_level):
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        with patch("sys.flags", SimpleNamespace(optimize=optimization_level)):
+            _check_asserts()
